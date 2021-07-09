204c9039670ce0b068664fa242b7e6193dfede99
saman <saman2000hoseini@gmail.com>
2021-07-09 05:21:17 +0430

    feat: add keep alive server

--- internal/app/container-scheduling/cmd/server/server.go
+++ internal/app/container-scheduling/cmd/server/server.go
@@ -1,12 +1,44 @@
 package server
 
 import (
+	"context"
 	"github.com/saman2000hoseini/container-scheduling/internal/app/container-scheduling/config"
+	"github.com/saman2000hoseini/container-scheduling/internal/app/container-scheduling/router"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main(cfg config.Config) {
-	//e := router.New(cfg)
+	e := router.New(cfg)
+
+	// place to define endpoints
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := e.Start(cfg.Server.Address); err != nil {
+			logrus.Fatalf("failed to start virtual-box management server: %s", err.Error())
+		}
+	}()
+
+	logrus.Info("virtual-box management server started!")
+
+	s := <-sig
+
+	logrus.Infof("signal %s received", s)
+
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.Server.GracefulTimeout)
+	defer cancel()
+
+	e.Server.SetKeepAlivesEnabled(false)
+
+	if err := e.Shutdown(ctx); err != nil {
+		logrus.Errorf("failed to shutdown virtual-box management server: %s", err.Error())
+	}
 }
 
 // Register registers server command for container-scheduling binary.
